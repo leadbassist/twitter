fcbd2f4d128abd1b50e523d608aa4dcc8e365bed
leadbassist <leadbassist@live.com>
2023-05-16 00:13:50 -0400

    navigation

---
+++ app/[id].tsx
@@ -0,0 +1,17 @@
+import { Text } from "react-native";
+import Tweet from "../components/Tweet";
+import tweets from "../assets/data/tweets";
+import { useSearchParams } from "expo-router";
+
+export default function TweetScreen() {
+  const { id } = useSearchParams();
+  // console.warn(id);
+
+  const tweet = tweets.find((t) => t.id === id);
+
+  if (!tweet) {
+    return <Text>Tweet {id} not found!</Text>;
+  }
+
+  return <Tweet tweet={tweet} />;
+}
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,36 +1,39 @@
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, Pressable } from "react-native";
 import { Entypo, EvilIcons } from "@expo/vector-icons";
 import IconButton from "../components/IconButton";
+import { Link } from "expo-router";
 
 import { TweetProps } from "../types";
 
 const Tweet = ({ tweet }: TweetProps) => {
   return (
-    <View style={styles.container}>
-      <Image src={tweet.user.image} style={styles.userImage} />
-      <View style={styles.mainContainer}>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.name}>{tweet.user.name}</Text>
-          <Text style={styles.userName}>{tweet.user.username}</Text>
-          <Text style={styles.userName}>• 2h</Text>
-          <Entypo
-            name="dots-three-horizontal"
-            size={16}
-            color="gray"
-            style={{ marginLeft: "auto" }}
-          />
+    <Link href={`/${tweet.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image src={tweet.user.image} style={styles.userImage} />
+        <View style={styles.mainContainer}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.name}>{tweet.user.name}</Text>
+            <Text style={styles.userName}>{tweet.user.username}</Text>
+            <Text style={styles.userName}>• 2h</Text>
+            <Entypo
+              name="dots-three-horizontal"
+              size={16}
+              color="gray"
+              style={{ marginLeft: "auto" }}
+            />
+          </View>
+          <Text style={styles.content}>{tweet.content}</Text>
+          {tweet.image && <Image src={tweet.image} style={styles.Image} />}
+          <View style={styles.footer}>
+            <IconButton icon="comment" text={tweet.numberOfComments} />
+            <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+            <IconButton icon="heart" text={tweet.numberOfLikes} />
+            <IconButton icon="chart" text={tweet.impressions || 0} />
+            <IconButton icon="share-apple" />
+          </View>
         </View>
-        <Text style={styles.content}>{tweet.content}</Text>
-        {tweet.image && <Image src={tweet.image} style={styles.Image} />}
-        <View style={styles.footer}>
-          <IconButton icon="comment" text={tweet.numberOfComments} />
-          <IconButton icon="retweet" text={tweet.numberOfRetweets} />
-          <IconButton icon="heart" text={tweet.numberOfLikes} />
-          <IconButton icon="chart" text={tweet.impressions || 0} />
-          <IconButton icon="share-apple" />
-        </View>
-      </View>
-    </View>
+      </Pressable>
+    </Link>
   );
 };
 
